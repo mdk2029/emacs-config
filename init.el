e16ac4b01b60facc8c94a16c82894c88184d85ac
MK <mdk.2029@gmail.com>
2020-02-26 16:22:55 -0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,33 @@
+;; .emacs.d/init.el
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-exec-path)
+(require 'init-ui)
+(require 'init-editing)
+(require 'init-navigation)
+(require 'init-miscellaneous)
+(require 'init-programming-common)
+(require 'init-python)
+(require 'init-rust)
+
+;;(require-package 'better-defaults)
+
+(provide 'init)
+
+;; User-Defined init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elpy smex rainbow-delimiters material-theme golden-ratio flycheck exec-path-from-shell company better-defaults atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
